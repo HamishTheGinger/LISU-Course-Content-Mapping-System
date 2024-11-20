24e5087307087c0b92a851f0e6aad996d3fa1eb8
Michael Baxendale <2788485b@student.gla.ac.uk>
2024-11-20 12:37:27 +0000

    Adding Basic Models

--- CCM Website/CCM Website/Migrations/20241119212210_InitalSetup.Designer.cs
+++ CCM Website/CCM Website/Migrations/20241119212210_InitalSetup.Designer.cs
@@ -3,6 +3,7 @@ using CCM_Website.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CCM_Website.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241119212210_InitalSetup")]
+    partial class InitalSetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
