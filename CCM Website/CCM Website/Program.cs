f7ab105280c22a6b74f219acc70e7bf55fdb7a64
Hamish Allan <2792542a@student.gla.ac.uk>
2024-11-20 16:53:10 +0000

    Adding Sqlite Database

--- CCM Website/CCM Website/Program.cs
+++ CCM Website/CCM Website/Program.cs
@@ -6,7 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlite(connectionString));
 
 var app = builder.Build();
 
